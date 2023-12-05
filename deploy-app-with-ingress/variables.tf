967a511eb0355b89ab4aef3c1ed3b4c051edc6a2
Nec2005 <necatiunal2005@gmail.com>
2023-12-05 10:39:52 +0000

    added secrets

--- deploy-app-with-ingress/variables.tf
+++ deploy-app-with-ingress/variables.tf
@@ -15,7 +15,7 @@ variable "service_dns_prefix" {
 
 variable "image_registry" {
   type = string
-  default = "607709576948.dkr.ecr.eu-west-2.amazonaws.com"  
+  default = "<ecr-repo>.eu-west-2.amazonaws.com"  
 }
 
 
